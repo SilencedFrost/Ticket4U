0204870ddaccab2f1194d6b84d1280a52b25e452
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-11-28 11:57:08 +0700

    fix: changed userId to id in AuthResponse to conform with current naming #9

--- backend/services/user-service/src/main/java/com/ticket4u/dto/auth/AuthResponse.java
+++ backend/services/user-service/src/main/java/com/ticket4u/dto/auth/AuthResponse.java
@@ -3,7 +3,7 @@ package com.ticket4u.dto.auth;
 import java.util.UUID;
 
 public record AuthResponse(
-        UUID userId,
+        UUID id,
         Integer roleId,
         String username,
         String email

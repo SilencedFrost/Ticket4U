47d3bc33d02db9799286a4c4d5b2fba5863784a7
Desmond Da Migu <tintin70189@gmail.com>
2025-12-03 19:22:33 +0700

    Add logout mapping

--- backend/services/user-service/src/main/java/com/ticket4u/controller/AuthController.java
+++ backend/services/user-service/src/main/java/com/ticket4u/controller/AuthController.java
@@ -5,13 +5,16 @@ import com.ticket4u.constant.TokenConstants;
 import com.ticket4u.dto.auth.AuthResponse;
 import com.ticket4u.dto.auth.LoginRequest;
 import com.ticket4u.dto.auth.internal.LoginResult;
+import com.ticket4u.dto.auth.internal.LogoutResult;
 import com.ticket4u.service.AuthService;
 import com.ticket4u.util.CookieUtil;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,7 +42,7 @@ public class AuthController {
     ) {
         log.debug("Login request received");
         // Cookie and header extraction, ready for fresh auth
-        String oldRefreshToken = cookieUtil.getCookie(request.getCookies(), TokenConstants.REFRESH_TOKEN.getCookieKey()).orElse(null);
+        String oldRefreshToken = cookieUtil.getCookieBody(request.getCookies(), TokenConstants.REFRESH_TOKEN.getCookieKey()).orElse(null);
         log.debug("Old refresh token {} present", oldRefreshToken == null ? "not" : "");
 
         String userAgent = request.getHeader(CommonKeys.USER_AGENT.getKey());
@@ -51,5 +54,29 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, loginResult.accessTokenCookie(), loginResult.refreshTokenCookie())
                 .body(loginResult.authResponse());
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        // Get old at and rt
+        Cookie[] cookies = request.getCookies();
+        Cookie oldRefreshTokenCookie = cookieUtil.getCookie(cookies, TokenConstants.REFRESH_TOKEN.getCookieKey()).orElse(null);
+        Cookie oldAccessTokenCookie = cookieUtil.getCookie(cookies, TokenConstants.ACCESS_TOKEN.getCookieKey()).orElse(null);
+
+        log.debug("Old refresh token {} present", oldRefreshTokenCookie == null ? "not" : "");
+        log.debug("Old access token {} present", oldAccessTokenCookie == null ? "not" : "");
+        // Get token values for section invalidation
+        String oldRefreshTokenValue = oldRefreshTokenCookie != null ? oldRefreshTokenCookie.getValue() : null;
+        String oldAccessTokenValue = oldAccessTokenCookie != null ? oldAccessTokenCookie.getValue() : null;
+
+        try {
+            // Pass old at and rt into authService.logout, get back the at and rt delete cookie
+            LogoutResult logoutResult = authService.logout(oldAccessTokenValue, oldRefreshTokenValue, oldAccessTokenCookie, oldRefreshTokenCookie);
+            return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, logoutResult.accessTokenCookie(), logoutResult.refreshTokenCookie()).build();
+        } catch (Exception e) {
+            log.error("Login request failed", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
     }
 }

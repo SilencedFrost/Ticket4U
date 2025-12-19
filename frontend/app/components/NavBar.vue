4d66013f0f8d69df79a55eba5d96d318376c4718
Nguyen Quoc Minh <thnrgbefv0987@gmail.com>
2025-12-19 18:16:20 +0700

    feat: started working on navbar #30

---
+++ frontend/app/components/NavBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <nav class="navbar navbar-expand-lg bg-reactive-primary shadow">
+    <div class="container-fluid">
+      <div class="navbar-brand">
+        <nuxt-link to="/">
+          <img src="/logo-primary.png" alt="" style="width: 40px" />
+        </nuxt-link>
+      </div>
+    </div>
+  </nav>
+</template>
--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+import NavBar from '~/components/NavBar.vue';
+</script>
 <template>
-  <div>
-    <nuxt-page />
+  <div class="d-flex flex-column h-100 flex-fill">
+    <header class="sticky-top" style="z-index: 9999">
+      <nav-bar />
+    </header>
+    <main class="d-flex flex-fill">
+      <nuxt-page />
+    </main>
+    <footer>footer</footer>
   </div>
 </template>

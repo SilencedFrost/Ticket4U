ca0fdd5451bf78a3a8df51a0b3f1bd1e9e0bee92
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-02-09 22:53:42 +0700

    feat: update database schema and seed data for events and organizers

--- backend/database/Ticket4U-User_Data.sql
+++ backend/database/Ticket4U-User_Data.sql
@@ -8,3 +8,29 @@ insert into public.users(id, email, role_id, username, password_hash, is_active,
 (uuidv7(), 'gatekeeper@gmail.com'    , 10, 'Gate keeper', '$argon2id$v=19$m=16384,t=2,p=1$NmYyQUZDaG9VcUZURXZ2bA$aQSvREKaeHY5EvoyEw1wiGhXrnVyA83OtaaHY/xvmr0', true, false, now()),
 (uuidv7(), 'supportAgent@gmail.com'  , 11, 'Support Agent', '$argon2id$v=19$m=16384,t=2,p=1$NmYyQUZDaG9VcUZURXZ2bA$aQSvREKaeHY5EvoyEw1wiGhXrnVyA83OtaaHY/xvmr0', true, false, now()),
 (uuidv7(), 'financeManager@gmail.com', 12, 'Finance Manager', '$argon2id$v=19$m=16384,t=2,p=1$NmYyQUZDaG9VcUZURXZ2bA$aQSvREKaeHY5EvoyEw1wiGhXrnVyA83OtaaHY/xvmr0', true, false, now())
+
+INSERT INTO public.users (id, email, role_id, username, password_hash, is_active, is_deleted, created_at)
+VALUES ('019bb098-c487-7bdb-9082-f51c9e8a9bb2', 'absolute@ticket4u.vn', 2, 'absolute_media', 'hash_pass', true, false, NOW())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.users (id, email, role_id, username, password_hash, is_active, is_deleted, created_at)
+VALUES ('019bb098-c487-7bdb-9082-f51c9e8a9cc5', 'catmouse@ticket4u.vn', 2, 'cat_mouse', 'hash_pass', true, false, NOW())
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.organizer (id, name, description, rating, logo_url)
+VALUES 
+(
+    '019bb098-c487-7bdb-9082-f51c9e8a9bb2', 
+    'Absolute Media', 
+    'Chuyên âm nhạc nghệ thuật cao cấp và giao hưởng.', 
+    4.80, 
+    'https://salt.tkbcdn.com/ts/ds/be/d4/a4/e6c34b2216009af82feba5f9bca782ad.jpg'
+),
+(
+    '019bb098-c487-7bdb-9082-f51c9e8a9cc5', 
+    'Cat&Mouse Live Music', 
+    'Phòng trà Cat&Mouse Live Music', 
+    4.80, 
+    'https://salt.tkbcdn.com/ts/ds/b4/f7/5f/dab59d5d6b39401a5b0864cbadb4a6f3.png'
+)
+ON CONFLICT (id) DO UPDATE SET logo_url = EXCLUDED.logo_url;

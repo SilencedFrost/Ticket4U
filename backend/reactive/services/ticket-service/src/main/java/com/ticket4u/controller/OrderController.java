fbe2ed5ec1a0e2f37d651a0d96e5e61fe4eaf531
SilencedFrost <thnrgbefv0987@gmail.com>
2026-02-10 20:39:53 +0700

    feat: added basic DTOs and completed API design

--- backend/reactive/services/ticket-service/src/main/java/com/ticket4u/controller/OrderController.java
+++ backend/reactive/services/ticket-service/src/main/java/com/ticket4u/controller/OrderController.java
@@ -3,15 +3,36 @@ package com.ticket4u.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/tickets")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 @Slf4j
 public class OrderController {
 
+    //TODO: Retrieves complete order information including ticket summary
+    @GetMapping("/{order-id}")
+    public ResponseEntity<?> getOrderInformation(@PathVariable("order-id") UUID orderId) {
+        return null;
+    }
+
+    //TODO:  Retrieves all tickets within a specific order
+    @GetMapping("/{order-id}/tickets")
+    public ResponseEntity<?> getOrderInformationWithTickets(@PathVariable("order-id") UUID orderId) {
+        return null;
+    }
+
+    //TODO: Converts a reservation into a purchased order. Only seats in user's reservation can be purchased.
+    @PostMapping
+    public ResponseEntity<?> createOrder() {
+        return null;
+    }
+
+    @DeleteMapping("/{order-id}")
+    public ResponseEntity<?> deleteOrder(@PathVariable("order-id") UUID orderId) {
+        return null;
+    }
 }
--- backend/reactive/services/ticket-service/src/main/java/com/ticket4u/controller/TicketController.java
+++ backend/reactive/services/ticket-service/src/main/java/com/ticket4u/controller/TicketController.java
@@ -3,10 +3,9 @@ package com.ticket4u.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/tickets")
@@ -14,22 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TicketController {
 
-    //TODO: Check all booked seats
-    @GetMapping
-    public ResponseEntity<?> getBookedSeats() {
+    //TODO: Retrieves individual ticket information
+    @GetMapping("{ticket-id}")
+    public ResponseEntity<?> getTicketInformation(@PathVariable("ticket-id") UUID ticketId) {
         return null;
     }
 
-    //TODO: Check seat vacancy
-    @GetMapping("/{seat-id}/event/{event-id}")
-    public ResponseEntity<?> getSeatStatus() {
+    //TODO: Updates ticket status (e.g., mark as used at venue entry, cancel individual ticket)
+    @PatchMapping("/{ticket-id}/status")
+    public ResponseEntity<?> updateTicketStatus(@PathVariable("ticket-id") UUID ticketId) {
         return null;
     }
-
-    //TODO: Reserve a seat
-    @PostMapping("/reserve")
-    public ResponseEntity<?> reserveSeat() {
-        return null;
-    }
-
 }

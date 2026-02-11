b6e34f558ed1641e465c33ac27e3e2cd9b852ca5
Van Dahy <vandahy@gmail.com>
2026-02-11 13:47:48 +0700

    chore: update nuxt.config.ts from feature/event

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -28,8 +28,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       authUrl: 'https://localhost:8080/api/v1/auth',
-      eventDetailUrl: 'https://localhost:8081/api/v1/public/events',
-      homeApiUrl: 'https://localhost:8081/api/v1/public/home',
+      ticketUrl: 'https://localhost:8081/api/v1',
+      ticketHealthUrl: 'https://localhost:8081/health',
     },
   },
 

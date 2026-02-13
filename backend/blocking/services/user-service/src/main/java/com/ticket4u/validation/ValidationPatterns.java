15f4b91ce7f343f68964d96a8e2c7025bc260798
SilencedFrost <thnrgbefv0987@gmail.com>
2026-02-13 16:12:28 +0700

    chore: updated phone number regex

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
@@ -3,7 +3,8 @@ package com.ticket4u.validation;
 public interface ValidationPatterns {
     String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_-]).{8,32}$";
 
-    String PHONE_NUMBER = "^(0\\d{9}|[1-9]\\d{8})$";
+    // May start with a 0, followed by 3, 5, 7, 8, 9, ends with another 8 numbers
+    String PHONE_NUMBER = "^0?(3|5|7|8|9)\\d{8}$";
 
     // Does not validate null values unlike @NotBlank
     String NON_BLANK = ".*\\S.*";

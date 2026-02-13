7f5dd989584d06e4c7c90ce3c7991461c65ffcb4
SilencedFrost <thnrgbefv0987@gmail.com>
2026-02-13 16:07:43 +0700

    chore: added comment on regex pattern for future maintainability

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
@@ -4,6 +4,7 @@ public interface ValidationPatterns {
     String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_-]).{8,32}$";
 
     String PHONE_NUMBER = "^(0\\d{9}|[1-9]\\d{8})$";
-    
+
+    // Does not validate null values unlike @NotBlank
     String NON_BLANK = ".*\\S.*";
 }

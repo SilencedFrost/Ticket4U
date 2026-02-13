40fb4d24441b5a7c90382c0041f7346de997cd5e
SilencedFrost <thnrgbefv0987@gmail.com>
2026-02-13 16:23:40 +0700

    chore: updated password validation pattern, added more characters into charset

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/validation/ValidationPatterns.java
@@ -1,7 +1,8 @@
 package com.ticket4u.validation;
 
 public interface ValidationPatterns {
-    String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*_-]).{8,32}$";
+    // from 8 to 32 chars long, have at least one of each category: lowercase, uppercase, number, characters from the set of !@$^*()-_=+[]{}\|;:",./?~`
+    String PASSWORD = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[!@$^*()_=+[\\]{}\\\\|;:\",.\\/?~`-]).{8,32}$";
 
     // May start with a 0, followed by 3, 5, 7, 8, 9, ends with another 8 numbers
     String PHONE_NUMBER = "^0?(3|5|7|8|9)\\d{8}$";

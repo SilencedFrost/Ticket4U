c3470c15d2ed4899dcd13c189fe541d971453235
Desmond Da Migu <tintin70189@gmail.com>
2026-02-28 21:41:45 +0700

    Revert "Merge branch 'feature/ticket-select' into feature/event-nghia"
    This reverts commit 27f1f35c370c479324141c87e33b73deb1044850, reversing
    changes made to 2687c605be7e6fef7a42fa01ca4c844968eb51e2.

--- backend/database/Ticket4U-User_Schema.sql
+++ backend/database/Ticket4U-User_Schema.sql
@@ -92,8 +92,5 @@ CREATE TABLE IF NOT EXISTS public.organizer
         REFERENCES public.users (id)
 );
 
-ALTER TABLE public.organizer 
-ADD COLUMN IF NOT EXISTS logo_url text;
-
 ALTER TABLE IF EXISTS public.organizer
     OWNER to postgres;
--- frontend/app/pages/ticket-select/types/event-payment.type.ts
+++
@@ -1,6 +0,0 @@
-export interface CartItem {
-  zoneId: string
-  name: string
-  quantity: number
-  price: number
-}

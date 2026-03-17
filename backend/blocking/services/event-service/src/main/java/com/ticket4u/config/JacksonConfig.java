f4003533848987fd9552ae19f4efc68ae40e5337
Desmond Da Migu <tintin70189@gmail.com>
2026-03-17 16:36:52 +0700

    refactor: fix JacksonConfig, no longer need to import Jackson or JSR-310

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/config/JacksonConfig.java
+++ backend/blocking/services/event-service/src/main/java/com/ticket4u/config/JacksonConfig.java
@@ -1,20 +1,18 @@
 package com.ticket4u.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.cfg.DateTimeFeature;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        return mapper;
+        return JsonMapper.builder()
+                .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
     }
 }

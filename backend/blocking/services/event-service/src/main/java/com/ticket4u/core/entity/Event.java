54dcb19001c1962a27e050c7f85769713158af73
SilencedFrost <thnrgbefv0987@gmail.com>
2026-03-20 01:01:55 +0700

    chore: replaced legacy explicit json casts with jdbc type, added hibernate jackson 3 mapper to allow hibernate to use jackson 3

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/core/entity/Event.java
+++ backend/blocking/services/event-service/src/main/java/com/ticket4u/core/entity/Event.java
@@ -3,10 +3,11 @@ package com.ticket4u.core.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnTransformer;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -118,6 +119,9 @@ public class Event {
     @JoinColumn(name = "venue_id")
     private Venue venue;
 
+    @JdbcTypeCode(SqlTypes.JSON)
+    private String layout;
+
     @Column(precision = 10, scale = 7)
     private BigDecimal longitude;
 
@@ -126,10 +130,6 @@ public class Event {
 
     @OneToMany(mappedBy = "event")
     private Set<EventSession> sessions = new LinkedHashSet<>();
-
-    @Column(name = "layout")
-    @ColumnTransformer(write = "?::jsonb")
-    private String layout;
 
     public enum EventStatus {
         EDITING,

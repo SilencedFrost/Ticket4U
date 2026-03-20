b19d2f938ba986142db597d99ada4dee297af152
Desmond Da Migu <tintin70189@gmail.com>
2026-03-20 13:30:19 +0700

    chore: changes in SecurityConfig
    - User with Roles: ORGANIZER_ADMIN, EVENT_MANAGER can use /api/v1/organizer/*

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/config/SecurityConfig.java
+++ backend/blocking/services/event-service/src/main/java/com/ticket4u/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package com.ticket4u.config;
 
+import com.ticket4u.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final CorsConfigurationSource corsConfigurationSource;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,12 +30,15 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        // Cho phép truy cập công khai vào tất cả các endpoint
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/*/public/**", "/api/*/auth/**", "/.well-known/**", "/api/v1/venues/**").permitAll()
+                        .requestMatchers("/api/v1/organizer/**")
+                        .hasAnyRole("EVENT_MANAGER", "ORGANIZER_ADMIN")
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }

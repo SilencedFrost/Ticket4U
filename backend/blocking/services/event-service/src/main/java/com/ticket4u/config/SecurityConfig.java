6d4e744323617c00f210cff52313faedde6f3634
Desmond Da Migu <tintin70189@gmail.com>
2026-03-21 23:33:25 +0700

    feat: fix hardcoded api url

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/config/SecurityConfig.java
+++ backend/blocking/services/event-service/src/main/java/com/ticket4u/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/*/public/**", "/.well-known/**").permitAll()
-                        .requestMatchers("/api/v1/**")
+                        .requestMatchers("/api/*/events/**", "/api/*/venues/**")
                         .hasAnyRole("EVENT_MANAGER", "ORGANIZER_ADMIN")
                         .anyRequest().authenticated()
                 )

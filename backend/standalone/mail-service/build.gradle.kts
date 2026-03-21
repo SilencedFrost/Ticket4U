08f1b7342ac7bcefee85e47c1a7168a300902fe4
SilencedFrost <thnrgbefv0987@gmail.com>
2026-03-22 00:41:17 +0700

    chore: removed strict requirement for exact jdk21

--- backend/standalone/mail-service/build.gradle.kts
+++ backend/standalone/mail-service/build.gradle.kts
@@ -8,12 +8,6 @@ group = "com.ticket4u"
 version = "0.0.1-SNAPSHOT"
 description = "Mail service for Ticket4U"
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
-}
-
 repositories {
 	mavenCentral()
 }

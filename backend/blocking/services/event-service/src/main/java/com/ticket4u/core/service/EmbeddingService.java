d73728af981b15478680364aa87b0c6e1d6aae39
SilencedFrost <thnrgbefv0987@gmail.com>
2026-03-23 23:24:22 +0700

    feat: added multiple bulk action method, strengthened logic, added more utility classes

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/core/service/EmbeddingService.java
+++
@@ -1,8 +0,0 @@
-package com.ticket4u.core.service;
-
-import java.util.List;
-
-public interface EmbeddingService {
-    List<Float> getDocumentEmbedding(String text);
-    List<Float> getQueryEmbedding(String query);
-}

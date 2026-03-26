c27e9d9604493a01ad0706b598a87020fba1379c
Desmond Da Migu <tintin70189@gmail.com>
2026-03-26 12:19:10 +0700

    chore: apply the correct path in global layout config

--- frontend/app/middleware/layout.global.ts
+++ frontend/app/middleware/layout.global.ts
@@ -3,8 +3,8 @@ export default defineNuxtRouteMiddleware((to) => {
 
   if (pathWithoutLocale.startsWith('/auth')) {
     setPageLayout('auth');
-  }  else if (pathWithoutLocale.startsWith('/select-ticket')) {
-  setPageLayout('blank')
+  } else if (pathWithoutLocale.startsWith('/select-ticket')) {
+    setPageLayout('blank')
   } else {
     setPageLayout('default');
   }

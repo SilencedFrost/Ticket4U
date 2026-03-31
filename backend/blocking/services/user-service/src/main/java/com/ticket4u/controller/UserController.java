04b6626bf79c46a44361a387cb177f758b57c075
vandahy <vandahy77@gmail.com>
2026-03-31 23:39:25 +0700

    feat: implement user account retrieval and update methods in UserController

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/controller/UserController.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/controller/UserController.java
@@ -3,12 +3,16 @@ package com.ticket4u.controller;
 import com.ticket4u.dto.user.ChangeInfoRequest;
 import com.ticket4u.dto.user.ChangePasswordRequest;
 import com.ticket4u.dto.user.UserSummaryResponse;
+import com.ticket4u.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -16,14 +20,18 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 public class UserController {
 
+    private final UserService userService;
+
     /**
      * GET /api/v1/users
      * To get current logged in account info summary
      * @return
      */
     @GetMapping
-    public ResponseEntity<UserSummaryResponse> getCurrentUser() {
-        return null;
+    public ResponseEntity<UserSummaryResponse> getCurrentUser(
+            @AuthenticationPrincipal UUID currentUserId
+    ) {
+        return ResponseEntity.ok(userService.getSummaryByIdOrThrow(currentUserId));
     }
 
     /**
@@ -33,8 +41,11 @@ public class UserController {
      * @return
      */
     @PatchMapping
-    public ResponseEntity<UserSummaryResponse> updateCurrentUser(@RequestBody @Valid ChangeInfoRequest request) {
-        return null;
+    public ResponseEntity<UserSummaryResponse> updateCurrentUser(
+            @AuthenticationPrincipal UUID currentUserId,
+            @RequestBody @Valid ChangeInfoRequest request
+    ) {
+        return ResponseEntity.ok(userService.updateInfo(currentUserId, request));
     }
 
     /**
@@ -44,7 +55,11 @@ public class UserController {
      * @return
      */
     @PatchMapping("/password")
-    public ResponseEntity<Void> changePassword(@RequestBody @Valid ChangePasswordRequest request) {
-        return null;
+    public ResponseEntity<Void> changePassword(
+            @AuthenticationPrincipal UUID currentUserId,
+            @RequestBody @Valid ChangePasswordRequest request
+    ) {
+        userService.changePassword(currentUserId, request);
+        return ResponseEntity.ok().build();
     }
 }

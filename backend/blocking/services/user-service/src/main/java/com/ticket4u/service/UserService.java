85e3425b829569d5f39c81455aced0ddc71033f1
vandahy <vandahy77@gmail.com>
2026-03-31 22:11:28 +0700

    feat: add InvalidPasswordException and implement password change validation in UserService

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/service/UserService.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/service/UserService.java
@@ -1,12 +1,18 @@
 package com.ticket4u.service;
 
+import com.ticket4u.dto.user.ChangeInfoRequest;
+import com.ticket4u.dto.user.ChangePasswordRequest;
 import com.ticket4u.dto.user.UserResponse;
+import com.ticket4u.dto.user.UserSummaryResponse;
 import com.ticket4u.entity.User;
+import com.ticket4u.exception.InvalidPasswordException;
 import com.ticket4u.exception.UserNotFoundException;
 import com.ticket4u.mapper.UserMapper;
 import com.ticket4u.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -17,6 +23,9 @@ public class UserService {
 
     private final UserMapper userMapper;
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    //Read
 
     public Optional<UserResponse> findById(UUID id) {
         return userRepository.findById(id).map(userMapper::toDTO);
@@ -34,4 +43,27 @@ public class UserService {
         );
     }
 
+    public UserSummaryResponse getSummaryByIdOrThrow(UUID id) {
+        User user = this.findEntityByIdOrThrow(id);
+        return userMapper.toSummaryResponse(user);
+    }
+
+    //Write
+
+    @Transactional
+    public UserSummaryResponse updateInfo(UUID userId, ChangeInfoRequest request) {
+        User user = findEntityByIdOrThrow(userId);
+        userMapper.updateFromChangeInfo(request, user);
+        return userMapper.toSummaryResponse(user);
+    }
+
+    @Transactional
+    public void changePassword(UUID userId, ChangePasswordRequest request) {
+        User user = findEntityByIdOrThrow(userId);
+
+        if (!passwordEncoder.matches(request.currentPassword(), user.getPasswordHash())){
+            throw new InvalidPasswordException("Current password is incorrect");
+        }
+        user.setPasswordHash(passwordEncoder.encode(request.newPassword()));
+    }
 }

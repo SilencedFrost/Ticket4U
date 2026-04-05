1c65634e633acdf5425e3a5a12ea9123a24f4899
SilencedFrost <thnrgbefv0987@gmail.com>
2026-04-05 23:00:18 +0700

    feat: added grid display

---
+++ frontend/app/pages/dev/event/display/grid.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import EventGrid from '~/features/event/components/layout/EventGrid.vue';
+import type { EventSummary } from '~/features/event/types/Event';
+
+const config = useRuntimeConfig();
+
+const eventList = ref<EventSummary[]>([]);
+
+async function getFeaturedEvents() {
+  try {
+    eventList.value = await $fetch(`${config.public.eventServiceUrl}/public/events/featured`, {
+      method: 'GET',
+    });
+  } catch (e) {
+    console.log(e);
+    eventList.value = [];
+  }
+}
+
+onMounted(() => getFeaturedEvents());
+</script>
+
+<template>
+  <event-grid :events="eventList" />
+</template>

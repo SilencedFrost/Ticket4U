9b4639d6d790cb1a434ab4d197d879337a045ae4
Desmond Da Migu <tintin70189@gmail.com>
2026-04-08 22:34:51 +0700

    refactor: update middleware to the correct url

--- frontend/app/middleware/layout.global.ts
+++ frontend/app/middleware/layout.global.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware((to) => {
   const routeName = String(to.name ?? '').replace(/___\w+$/, '');
 
   if (routeName.startsWith('auth')) setPageLayout('auth');
-  if (routeName.startsWith('event') && routeName.includes('book/seats')) setPageLayout('blank');
+  if (routeName.includes('event') && routeName.includes('book-seats')) setPageLayout('blank');
   if (routeName.startsWith('settings')) to.meta.hideFooter = true;
 });

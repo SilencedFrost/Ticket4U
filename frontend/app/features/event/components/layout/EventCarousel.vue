4e99904195e930ec046477f1b27f9b3979803ebc
SilencedFrost <thnrgbefv0987@gmail.com>
2026-04-12 22:41:40 +0700

    feat: added chevron offset interpolation based on screen width

--- frontend/app/features/event/components/layout/EventCarousel.vue
+++ frontend/app/features/event/components/layout/EventCarousel.vue
@@ -2,7 +2,7 @@
 import type { EventSummary } from '../../types/Event';
 import CarouselWrapper from '~/features/components/CarouselWrapper.vue';
 import EventCard from '../core/EventCard.vue';
-import { breakpointsBootstrapV5, useBreakpoints } from '@vueuse/core';
+import { breakpointsBootstrapV5, useBreakpoints, useWindowSize } from '@vueuse/core';
 
 interface Props {
   events: EventSummary[];
@@ -13,30 +13,50 @@ withDefaults(defineProps<Props>(), {
   wrapAround: true,
 });
 
-const breakpoints = useBreakpoints(breakpointsBootstrapV5);
+const interpolatePoints = ref([
+  { x: 320, y: 62 },
+  { x: 480, y: 40 },
+  { x: 540, y: 40 },
+  // md
+  { x: 767, y: 28 },
+  { x: 768, y: 40 },
+  // lg
+  { x: 991, y: 30 },
+  { x: 992, y: 50 },
+  //xxl
+  { x: 1399, y: 35 },
+  { x: 1400, y: 47 },
+  // really big?
+  { x: 2000, y: 30 },
+  { x: 3000, y: 22 },
+]);
 
-const xxl = breakpoints.greaterOrEqual('xxl');
-const md = breakpoints.greaterOrEqual('md');
+const breakpoints = useBreakpoints(breakpointsBootstrapV5);
+const { interpolate } = useInterpolation(interpolatePoints);
+const { width } = useWindowSize();
+
+const isXxl = breakpoints.greaterOrEqual('xxl');
+const isMd = breakpoints.greaterOrEqual('md');
 
 const visibleCount = computed(() => {
-  if (xxl.value) return 4;
-  if (md.value) return 3;
+  if (isXxl.value) return 4;
+  if (isMd.value) return 3;
   return 2;
 });
+
+const offset = computed(() => interpolate(width.value));
 </script>
 
 <template>
-  <div>
-    <carousel-wrapper
-      :items="events"
-      :visible-count="visibleCount"
-      mode="carousel"
-      :wrap-around="wrapAround"
-      :chevron-options="{ offset: 50 }"
-    >
-      <template #item="{ item }">
-        <event-card :event="item" />
-      </template>
-    </carousel-wrapper>
-  </div>
+  <carousel-wrapper
+    :items="events"
+    :visible-count="visibleCount"
+    mode="carousel"
+    :wrap-around="wrapAround"
+    :chevron-options="{ offset, height: isMd ? 55 : 50 }"
+  >
+    <template #item="{ item }">
+      <event-card :event="item" />
+    </template>
+  </carousel-wrapper>
 </template>

6e8ed863923ad9222d1b2a31b3080de777ada75a
SilencedFrost <thnrgbefv0987@gmail.com>
2026-04-12 23:44:13 +0700

    feat: added event hero carousel, fixed 1 visible item problem in carousel

---
+++ frontend/app/features/event/components/layout/EventHeroCarousel.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { EventSummary } from '../../types/Event';
+import CarouselWrapper from '~/features/components/CarouselWrapper.vue';
+import EventHero from '../core/EventHero.vue';
+import { breakpointsBootstrapV5, useBreakpoints } from '@vueuse/core';
+
+interface Props {
+  events: EventSummary[];
+  wrapAround?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  wrapAround: true,
+});
+
+const breakpoints = useBreakpoints(breakpointsBootstrapV5);
+
+const isMd = breakpoints.greaterOrEqual('md');
+</script>
+
+<template>
+  <carousel-wrapper
+    :items="events"
+    :visible-count="1"
+    mode="carousel"
+    :wrap-around="true"
+    :chevron-options="{ height: isMd ? 55 : 50 }"
+  >
+    <template #item="{ item }">
+      <event-hero :event="item" />
+    </template>
+  </carousel-wrapper>
+</template>

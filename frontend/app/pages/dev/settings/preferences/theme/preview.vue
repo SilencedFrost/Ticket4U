20187c7f250660bc4e9a2f932a827b28a2f8de0f
SilencedFrost <thnrgbefv0987@gmail.com>
2026-04-12 21:04:52 +0700

    feat: added standalone component for theme preview

---
+++ frontend/app/pages/dev/settings/preferences/theme/preview.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import ThemePreview from '~/features/settings/preferences/components/ThemePreview.vue';
+</script>
+
+<template>
+  <div class="container-xxl p-2">
+    <div class="card p-3">
+      <div class="row g-2">
+        <div class="col"><ThemePreview theme="light" /></div>
+        <div class="col"><ThemePreview theme="dark" /></div>
+      </div>
+    </div>
+  </div>
+</template>

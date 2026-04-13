8cf76c6024b6fc2c9ba873efae459bf85ab147ee
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-04-13 10:28:58 +0700

    feat: add zones field to EventSessionResponse

--- backend/blocking/services/event-service/src/main/java/com/ticket4u/core/dto/EventSessionResponse.java
+++ backend/blocking/services/event-service/src/main/java/com/ticket4u/core/dto/EventSessionResponse.java
@@ -3,12 +3,14 @@ package com.ticket4u.core.dto;
 import com.ticket4u.core.entity.EventSession;
 
 import java.time.OffsetDateTime;
+import java.util.Set;
 
 public record EventSessionResponse(
         String id,
         OffsetDateTime startDate,
         OffsetDateTime endDate,
         EventSession.SessionStatus status,
-        String name
+        String name,
+        Set<ZoneResponse> zones
 ) {
 }

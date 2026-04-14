724e677f47d41e6b2e5ea196b62c313d72f61b66
vandahy <vandahy77@gmail.com>
2026-04-14 09:42:32 +0700

    refactor: remove EmailAlreadyInUseException and its handler from AuthExceptionHandler

--- backend/blocking/services/user-service/src/main/java/com/ticket4u/advice/AuthExceptionHandler.java
+++ backend/blocking/services/user-service/src/main/java/com/ticket4u/advice/AuthExceptionHandler.java
@@ -73,10 +73,4 @@ public class AuthExceptionHandler {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body(Map.of("message", e.getMessage()));
     }
-
-    @ExceptionHandler(EmailAlreadyInUseException.class)
-    public ResponseEntity<?> handleEmailAlreadyInUse(EmailAlreadyInUseException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(Map.of("message", e.getMessage()));
-    }
 }
--- backend/blocking/services/user-service/src/main/java/com/ticket4u/exception/EmailAlreadyInUseException.java
+++
@@ -1,7 +0,0 @@
-package com.ticket4u.exception;
-
-public class EmailAlreadyInUseException extends RuntimeException {
-    public EmailAlreadyInUseException() {
-        super("auth.error.email_already_used");
-    }
-}

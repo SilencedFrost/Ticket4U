8fa525c2300a6fa27de4b930419024f1ee5c64a9
SilencedFrost <thnrgbefv0987@gmail.com>
2026-04-26 23:05:52 +0700

    feat: added typing for http methods, prepared useEventApi.ts for expansion, fixed carousel bug

--- frontend/app/features/homepage/components/HomePage.vue
+++ frontend/app/features/homepage/components/HomePage.vue
@@ -14,9 +14,12 @@ const { data: eventList } = useFeaturedEvents(15);
 // TODO: add section displaying top 3 categories that the user have bought into
 // TODO: add section to display other events
 
-const sectionList = ref([
-  { key: 'homepage.section.popular', eventList: eventList.value.slice(1, 5), wrapAround: true },
-]);
+// TODO: dynamic sectioning (get from backend)
+const sectionList = computed(() => {
+  return [
+    { key: 'homepage.section.popular', eventList: eventList.value.slice(1, 5), wrapAround: true },
+  ];
+});
 
 const handleEventClick = (eventId: string) => {
   navigateTo(localePath(`/event/${eventId}`));
@@ -32,14 +35,15 @@ const handleEventClick = (eventId: string) => {
         :wrap-around="true"
         @event-click="handleEventClick"
       />
-      <event-section
-        v-for="section in sectionList"
-        :key="section.key"
-        :events="section.eventList"
-        :title="$t(section.key)"
-        :wrap-around="section.wrapAround"
-        @event-click="handleEventClick"
-      />
+
+      <template v-for="section in sectionList" :key="section.key">
+        <event-section
+          :events="section.eventList"
+          :title="$t(section.key)"
+          :wrap-around="section.wrapAround"
+          @event-click="handleEventClick"
+        />
+      </template>
       <hr />
       <h3 class="fw-bold mb-3">Other events</h3>
       <event-grid :events="eventList.slice(9, 15)" @event-click="handleEventClick" />

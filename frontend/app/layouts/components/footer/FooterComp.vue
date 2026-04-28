5cd29d6b4693cf35a8ffcd2e6e672ab396317602
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-04-28 22:56:17 +0700

    feat: implement i18n translate for footer

--- frontend/app/layouts/components/footer/FooterComp.vue
+++ frontend/app/layouts/components/footer/FooterComp.vue
@@ -8,7 +8,7 @@
           </div>
 
           <div class="d-flex flex-column">
-            <h6 class="text-uppercase fw-bold mb-3 small">Follow Us</h6>
+            <h6 class="text-uppercase fw-bold mb-3 small">{{ $t('common.follow_us') }}</h6>
             <div class="d-flex gap-2">
               <a
                 href="#"
@@ -39,25 +39,23 @@
         </div>
 
         <div class="col-12 col-md-6">
-          <div>
-            <h5 class="fw-bold mb-3">About Us</h5>
+          <div class="mb-4">
+            <h5 class="fw-bold mb-3">{{ $t('common.about_us') }}</h5>
             <p class="text-reactive-secondary small lh-base">
-              Ticket4U is your ultimate platform for discovering and booking events. From concerts
-              to conferences, we bring you the best experiences at your fingertips. Join thousands
-              of event enthusiasts today.
+              {{ $t('brand.description') }}
             </p>
           </div>
         </div>
 
         <div class="col-12 col-md-3">
           <div class="mb-4">
-            <h5 class="fw-bold mb-3">Contact</h5>
+            <h5 class="fw-bold mb-3">{{ $t('common.contact') }}</h5>
             <div class="mb-3">
-              <p class="text-reactive-secondary mb-1 small">Call:</p>
+              <p class="text-reactive-secondary mb-1 small">{{ $t('common.phone') }}:</p>
               <p class="text-white fw-bold mb-0 small">0912345678</p>
             </div>
             <div>
-              <p class="text-reactive-secondary mb-1 small">Email:</p>
+              <p class="text-reactive-secondary mb-1 small">{{ $t('common.email') }}:</p>
               <p class="text-white fw-bold mb-0 small">support@ticket4u.com</p>
             </div>
           </div>
@@ -68,7 +66,7 @@
 
       <div class="row">
         <div class="col-12 text-center">
-          <p class="text-reactive-secondary mb-0 small">© 2026 Ticket4U. All rights reserved.</p>
+          <p class="text-reactive-secondary mb-0 small">{{ $t('brand.copyright') }}</p>
         </div>
       </div>
     </div>

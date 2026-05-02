e49a91ea4b3db7757cda6e83ff8f46036b4864f6
trungnghia-coder <nguyentrunghia0907@gmail.com>
2026-05-02 21:39:30 +0700

    refactor: add footer desktop layout

--- frontend/app/layouts/components/footer/FooterComp.vue
+++ frontend/app/layouts/components/footer/FooterComp.vue
@@ -1,198 +1,30 @@
+<script setup lang="ts">
+import FooterDesktop from './FooterDesktop.vue';
+import FooterMobile from './FooterMobile.vue';
+import { FOOTER_BOTTOM_LINKS } from './footerData';
+</script>
+
 <template>
   <footer class="bg-reactive-dark text-white pt-5 pb-4">
     <div class="container-lg">
-      <div class="row g-5">
-        <div class="col-6 col-lg-3">
-          <h6 class="mb-4 text-uppercase fw-bold">
-            <span class="d-inline-block pb-3 border-bottom border-3 border-primary">
-              {{ $t('common.events') }}
-            </span>
-          </h6>
-          <ul class="list-unstyled d-flex flex-column gap-3 mb-0">
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.all_events') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.popular_events') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.upcoming_events') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.nearby_events') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.free_events') }}</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div class="d-md-none">
+        <footer-mobile />
+      </div>
 
-        <div class="col-6 col-lg-3">
-          <h6 class="mb-4 text-uppercase fw-bold">
-            <span class="d-inline-block pb-3 border-bottom border-3 border-primary">
-              {{ $t('common.about_us') }}
-            </span>
-          </h6>
-          <ul class="list-unstyled d-flex flex-column gap-3 mb-0">
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.our_story') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.team') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.careers') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.press_media') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.contact_us') }}</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-6 col-lg-3">
-          <h6 class="mb-4 text-uppercase fw-bold">
-            <span class="d-inline-block pb-3 border-bottom border-3 border-primary">
-              {{ $t('footer.partners') }}
-            </span>
-          </h6>
-          <ul class="list-unstyled d-flex flex-column gap-3 mb-0">
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.become_partner') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.sponsorship') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.partner_directory') }}</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
-              >
-                <span aria-hidden="true">•</span>
-                <span>{{ $t('footer.menu.partner_login') }}</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <div class="col-6 col-lg-3">
-          <h6 class="mb-4 text-uppercase fw-bold">
-            <span class="d-inline-block pb-3 border-bottom border-3 border-primary">
-              {{ $t('common.contact') }}
-            </span>
-          </h6>
-
-          <div class="mb-3">
-            <div class="small text-uppercase fw-bold text-reactive-secondary mb-1">
-              {{ $t('footer.labels.phone') }}
-            </div>
-            <div class="fw-bold">0912 345 678</div>
-          </div>
-          <div>
-            <div class="small text-uppercase fw-bold text-reactive-secondary mb-1">
-              {{ $t('footer.labels.email') }}
-            </div>
-            <a
-              href="mailto:supportagent@gmail.com"
-              class="link-primary text-decoration-none fw-bold"
-            >
-              supportagent@gmail.com
-            </a>
-          </div>
-        </div>
+      <div class="d-none d-md-block">
+        <footer-desktop />
       </div>
 
       <hr class="text-white-50 opacity-25" />
 
       <div class="d-flex justify-content-end gap-4">
-        <a href="#" class="text-clickable user-select-auto text-decoration-none">
-          {{ $t('footer.links.privacy_policy') }}
-        </a>
-        <a href="#" class="text-clickable user-select-auto text-decoration-none">
-          {{ $t('footer.links.terms_of_use') }}
+        <a
+          v-for="link in FOOTER_BOTTOM_LINKS"
+          :key="link.labelKey"
+          :href="link.href"
+          class="text-clickable user-select-auto text-decoration-none"
+        >
+          {{ $t(link.labelKey) }}
         </a>
       </div>
     </div>
---
+++ frontend/app/layouts/components/footer/FooterDesktop.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { FOOTER_CONTACT, FOOTER_SECTIONS } from './footerData';
+</script>
+
+<template>
+  <div class="row g-5">
+    <div v-for="section in FOOTER_SECTIONS" :key="section.id" class="col-6 col-lg-3">
+      <h6 class="mb-4 text-uppercase fw-bold">
+        <span class="d-inline-block pb-3 border-bottom border-3 border-primary">
+          {{ $t(section.titleKey) }}
+        </span>
+      </h6>
+
+      <ul v-if="section.type === 'links'" class="list-unstyled d-flex flex-column gap-3 mb-0">
+        <li v-for="item in section.items" :key="item.labelKey">
+          <a
+            :href="item.href"
+            class="text-clickable user-select-auto text-decoration-none d-inline-flex align-items-center gap-2"
+          >
+            <span aria-hidden="true">•</span>
+            <span>{{ $t(item.labelKey) }}</span>
+          </a>
+        </li>
+      </ul>
+
+      <div v-else>
+        <div class="mb-3">
+          <div class="small text-uppercase fw-bold text-reactive-secondary mb-1">
+            {{ $t(FOOTER_CONTACT.phoneLabelKey) }}
+          </div>
+          <div class="fw-bold">{{ FOOTER_CONTACT.phoneText }}</div>
+        </div>
+
+        <div>
+          <div class="small text-uppercase fw-bold text-reactive-secondary mb-1">
+            {{ $t(FOOTER_CONTACT.emailLabelKey) }}
+          </div>
+          <a
+            :href="`mailto:${FOOTER_CONTACT.email}`"
+            class="link-primary text-decoration-none fw-bold"
+          >
+            {{ FOOTER_CONTACT.email }}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

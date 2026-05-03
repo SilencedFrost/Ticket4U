b2044f691a6b7766028b9ab73c211e03b9d7f018
La Ngoc Hung <langochungdev@gmail.com>
2026-05-03 19:03:20 +0700

    feat(auth): enhance login redirection to support loginRequired query parameter

--- frontend/app/middleware/layout.global.ts
+++ frontend/app/middleware/layout.global.ts
@@ -5,13 +5,21 @@ export default defineNuxtRouteMiddleware((to) => {
   if (routeName.includes('event') && routeName.includes('book-seats')) setPageLayout('blank');
   if (routeName.startsWith('settings')) to.meta.hideFooter = true;
 
-    const protectedRoutes = ['settings-account', 'settings-security'];
+  const protectedRoutes = ['settings-account', 'settings-security'];
   if (protectedRoutes.includes(routeName)) {
     const { isLoggedIn } = storeToRefs(useUserStore());
     if (!isLoggedIn.value) {
       const localePath = useLocalePath();
       // Giữ lại path gốc để modal biết cần redirect về đâu sau login
       return navigateTo(localePath(`/settings/preferences?loginRequired=${to.path}`));
+    }
+  }
+
+  if (routeName === 'payment' || to.path.includes('/book/checkout')) {
+    const { isLoggedIn } = storeToRefs(useUserStore());
+    if (!isLoggedIn.value) {
+      const localePath = useLocalePath();
+      return navigateTo(localePath(`/auth/login?loginRequired=${to.path}`));
     }
   }
 });
